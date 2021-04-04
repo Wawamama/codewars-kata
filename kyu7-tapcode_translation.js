909a4bf626145bc2ee4958f565d08109f793ee33
Marion Saul <77323113+Wawamama@users.noreply.github.com>
2021-04-04 13:01:59 +0200

    Create kyu7-tapcode_translation.js
    Tap Code Translation
    Tap code is a way to communicate using a series of taps and pauses for each letter. In this kata, we will use dots . for the taps and whitespaces for the pauses.
    
    The number of taps needed for each letter matches its coordinates in the following polybius square (note the c/k position). Then you "tap" the row, a pause, then the column. Each letter is separated from others with a pause too.
    
       1  2  3  4  5
    1  A  B C\K D  E
    2  F  G  H  I  J
    3  L  M  N  O  P
    4  Q  R  S  T  U
    5  V  W  X  Y  Z
    Input:
    A lowercase string of a single word (no whitespaces or punctuation, only letters).
    
    Output:
    The encoded string as taps and pauses.
    
    Examples
    text = "dot"
      "D" = (1, 4) = ". ...."
      "O" = (3, 4) = "... ...."
      "T" = (4, 4) = ".... ...."
      
    output: ". .... ... .... .... ...."
    
    
    "example" -> ". ..... ..... ... . . ... .. ... ..... ... . . ....."
    "more"    -> "... .. ... .... .... .. . ....."

---
+++ kyu7-tapcode_translation.js
@@ -0,0 +1,50 @@
+// /!\ NOT FINISHED : This function doesn't work with C and K's
+
+function tapCodeTranslation(text) {
+  let letters = text.toUpperCase().split('');
+  let finalString = '';
+    
+  const lines = [
+    ['A', 'B', 'C/K', 'D', 'E'],
+    ['F', 'G', 'H', 'I', 'J'],
+    ['L', 'M', 'N', 'O', 'P'],
+    ['Q', 'R', 'S', 'T', 'U'],
+    ['V', 'W', 'X', 'Y', 'Z']
+  ];
+  
+  const columns = [
+    ['A', 'F', 'L', 'Q', 'V'],
+    ['B', 'G', 'M', 'R', 'W'],
+    ['C/K', 'H', 'N', 'S', 'X'],
+    ['D', 'I', 'O', 'T', 'Y'],
+    ['E', 'J', 'P', 'U', 'Z']   
+  ];
+  
+  letters.forEach( (letter) => {
+    let lineIndex, columnIndex, lineDots = '.', columnDots ='.';
+    
+    //find corresponding line
+    for (let i=0; i<lines.length; i++) {
+      if (lines[i].includes(letter)) {
+        lineIndex = lines.indexOf(lines[i])+1
+        
+      }
+    }
+    // find corresponding column
+    for (let j=0; j<columns.length; j++) {
+      if (columns[j].includes(letter)) {
+        columnIndex = columns.indexOf(columns[j])+1
+      }
+    }
+    
+    // add the taps for each letter
+    lineDots = lineDots.repeat(lineIndex);
+    columnDots = columnDots.repeat(columnIndex);
+    finalString += lineDots + ' ' + columnDots + ' ';
+    
+  })
+ 
+  return(finalString.trim())
+ 
+  
+}
