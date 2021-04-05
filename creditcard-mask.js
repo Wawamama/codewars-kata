51ff2c6953226031f73b837d6af658cbc2c4126b
Marion Saul <77323113+Wawamama@users.noreply.github.com>
2021-04-05 19:47:52 +0200

    Update creditcard-mask.js

--- creditcard-mask.js
+++ creditcard-mask.js
@@ -1,4 +1,5 @@
 function maskify(cc) {
-  let regex = /\d(?=\d{4})/g;
+  //let regex = /\d(?=\d{4})/g; //would work if only digits are passed
+  let regex = /.(?=.{4})/g;  //should match any character
   return cc.replace(regex, '#');
 }
